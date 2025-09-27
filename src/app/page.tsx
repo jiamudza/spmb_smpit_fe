deb326a8ef12f7c365a2317390c41a66e7cbd79c
jiamudza <ajimasbagus12@gmail.com>
2025-09-27 10:10:06 +0700

    dashboar modify

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='absolute-center text-5xl font-bold text-center'>Ini halaman depan</div>
+  )
+}
+
+export default Dashboard
